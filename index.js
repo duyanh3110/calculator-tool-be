8c79d792ea63e2746fb1850a87e5d0dc9b5e2436
duyanh3110 <duyanh19962012@gmail.com>
2024-08-01 10:08:52 +0300

    Add handle db files

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import helmet from "helmet";
 import compression from "compression";
 import cors from "cors";
 import { writeFileSync, readFileSync } from "fs";
+import { formatDate, createDataFile } from "./utils.js";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 const port = 3000;
@@ -18,7 +20,11 @@ app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const allowList = ["https://calculator-tool-nail.netlify.app"];
+const allowList = [
+    "https://calculator-tool-nail.netlify.app",
+    "localhost:5173",
+];
+
 const corsOptionsDelegate = function (req, callback) {
     let corsOptions;
     if (allowList.indexOf(req.header("Origin")) !== -1) {
@@ -29,7 +35,14 @@ const corsOptionsDelegate = function (req, callback) {
     callback(null, corsOptions); // callback expects two parameters: error and options
 };
 
-const dataPath = "./data.json";
+const formatDateStr = formatDate();
+const fileName = `${formatDateStr}.json`;
+const dataPath = `./db/${fileName}`;
+
+app.get("/v1/api/files", cors(corsOptionsDelegate), async (req, res) => {
+    await createDataFile("db", fileName);
+    res.status(200).send("OK");
+});
 
 app.get("/v1/api/services", cors(corsOptionsDelegate), (req, res) => {
     let data = readFileSync(dataPath, { encoding: "utf8", flag: "r" });
@@ -38,12 +51,25 @@ app.get("/v1/api/services", cors(corsOptionsDelegate), (req, res) => {
 
 app.post("/v1/api/services", cors(corsOptionsDelegate), (req, res) => {
     const newService = req.body;
+    newService.id = uuidv4();
+
     let data = readFileSync(dataPath, { encoding: "utf8", flag: "r" });
     const newData = JSON.parse(data);
     newData.push(newService);
+
     writeFileSync(dataPath, JSON.stringify(newData, null, 2), "utf8");
 
     res.status(200).json(newService);
+});
+
+app.delete("/v1/api/services/:id", cors(corsOptionsDelegate), (req, res) => {
+    let data = readFileSync(dataPath, { encoding: "utf8", flag: "r" });
+    let newData = JSON.parse(data);
+    newData = newData.filter((data) => data.id !== req.params.id);
+
+    writeFileSync(dataPath, JSON.stringify(newData, null, 2), "utf8");
+
+    res.status(200).json("OK");
 });
 
 app.listen(port, () => {
